b3badd0e985aca6969addd589d8bc0b1bd688393
Eric Posser <eric_alegrete2002@hotmail.com>
2023-10-17 20:24:06 -0300

    Update SQLParaAtividadePessoa.sql

--- SQLParaAtividadePessoa.sql
+++ SQLParaAtividadePessoa.sql
@@ -1,3 +1,7 @@
+create database aula;
+
+use aula;
+
 CREATE TABLE pessoa (
   id int primary key AUTO_INCREMENT,
   nome varchar(255),
@@ -6,3 +10,13 @@ CREATE TABLE pessoa (
 );
 
 select * from pessoa;
+
+create table veiculo(
+	id int auto_increment primary key,
+    modelo varchar(30),
+    placa varchar(7),
+    id_pessoa int not null,
+    foreign key (id_pessoa) references pessoa(id)
+);
+
+select * from veiculo;
