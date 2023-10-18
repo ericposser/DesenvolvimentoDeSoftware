f36d3033c77a193f36cff1d688c6d68a494f2b13
Eric Posser <eric_alegrete2002@hotmail.com>
2023-10-18 19:17:07 -0300

    Update SQLParaAtividadeEscola.sql

--- SQLParaAtividadeEscola.sql
+++ SQLParaAtividadeEscola.sql
@@ -1,3 +1,7 @@
+create database escola;
+
+use escola;
+
 create table aluno(
 	id int auto_increment primary key,
     nome varchar(50),
@@ -12,5 +16,15 @@ create table professor(
     disciplina varchar(50)
 );
 
+create table disciplina(
+	id int auto_increment primary key,
+	nome varchar(30),
+	carga int,
+	id_professor int not null,
+
+	foreign key (id_professor) references id(professor)
+);
+
 select * from aluno;
 select * from professor;
+select * from disciplina;
